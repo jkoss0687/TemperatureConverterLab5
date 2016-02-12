d31a884539f1ab93c760f1b25c0c57c4992afdc8
Jessica <jkoss0687@yahoo.com>
2016-02-12 16:29:00 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// var ftemp = 65;
+// var cel = 100;
+
+// var celsius = ftemp -32*(5/9);
+// console.log(celsius + " Temp converted to Celsius");
+
+// var fah = cel +32*(9/5);
+// console.log(fah + " Temp converted to Fahrenheit");
+
+
+
+var ftemp = prompt ("What is todays temperature");
+var ctemp = ftemp -32*(5/9);
+document.write("The calculation from Fahrenheit to celsius is " + ctemp); 
